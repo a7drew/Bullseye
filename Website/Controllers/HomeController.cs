7e189b285f83623892579f9d92e40ccee2e3b9a3
Andrew Hay <andrewcameronhay@hotmail.com>
2014-02-14 09:59:42 -0800

    Initial commit

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static string _jsessionId = Guid.NewGuid().ToString();
+
+        public string Index()
+        {
+            return _jsessionId;
+        }
+
+        [HttpPost]
+        [AllowCrossSiteJson]
+        public void Edit(string id, FormCollection collection)
+        {
+            _jsessionId = id;
+        }
+    }
+
+    public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Website/Scripts/custom.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var http = new XMLHttpRequest();
+    var url = "http://localhost:21267/home/edit/hoo";
+    var params = "foo=moo";
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // http.setRequestHeader("Content-length", params.length);
+    // http.setRequestHeader("Connection", "close");
+    
+    http.send(params);
+})();
